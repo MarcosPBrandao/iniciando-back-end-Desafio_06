f2e215299e67300ad4923a126bf7d61aafac0165
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-06-27 20:14:54 -0300

    Commit Second

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,8 +11,18 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = this.find();
+    const {income,outcome} = (await transactions).reduce((resultado,transaction) => {
+      if (transaction.type == 'income') { resultado.income += Number(transaction.value) } 
+      if (transaction.type == 'outcome') { resultado.outcome += Number(transaction.value) }
+      return resultado;
+    },{
+      income: 0,
+      outcome: 0,
+      total: 0
+    });
+    const total = income - outcome; 
+    return {income, outcome, total };   
   }
 }
-
 export default TransactionsRepository;
